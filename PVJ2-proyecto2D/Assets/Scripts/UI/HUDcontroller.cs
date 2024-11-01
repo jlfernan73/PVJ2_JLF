846a0b1722b7d07d2fbe8fa2ebf473f8018bff5b
Jose Luis <jlfernan_73@yahoo.com.ar>
2024-10-31 22:36:51 -0300

    Se inicia Nivel 7
    Se agregaron algunos efectos en las barras de energía y combustible

--- PVJ2-proyecto2D/Assets/Scripts/UI/HUDcontroller.cs
+++ PVJ2-proyecto2D/Assets/Scripts/UI/HUDcontroller.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class HUDcontroller : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI miTexto;
-    [SerializeField] Transform barraEnergia;
-    [SerializeField] Transform barraCombustible;
+    [SerializeField] GameObject barraEnergia;
+    [SerializeField] GameObject barraCombustible;
     [SerializeField] GameObject GasolinaItem;
     [SerializeField] GameObject NitroItem;
     [SerializeField] GameObject BumperItem;
+    private Animator barraEnergiaAnimator;
+    private Animator barraCombustibleAnimator;
+
+    public void Start()
+    {
+        barraEnergiaAnimator = barraEnergia.GetComponent<Animator>();
+        barraCombustibleAnimator = barraCombustible.GetComponent<Animator>();
+        for (int i=1;i<4;i++)
+        {
+            ActualizarEstadoObjeto(i, false);
+        }
+    }
 
     public void ActualizarTextoHUD(string nuevoTexto)
     {
@@ -19,12 +32,14 @@ public class HUDcontroller : MonoBehaviour
 
     public void ActualizarBarraEnergia(float energia)
     {
-        barraEnergia.localScale = new Vector3(1, energia / 100f, 1);
+        barraEnergia.transform.localScale = new Vector3(1, energia / 100f, 1);
+        barraEnergiaAnimator.SetFloat("Energia", energia);
     }
 
     public void ActualizarBarraCombustible(float combustible)
     {
-        barraCombustible.localScale = new Vector3(1, combustible / 100f, 1);
+        barraCombustible.transform.localScale = new Vector3(1, combustible / 100f, 1);
+        barraCombustibleAnimator.SetFloat("Combustible", combustible);
     }
 
     public void ActualizarEstadoObjeto(int objeto, bool estado)
